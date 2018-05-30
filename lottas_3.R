cc31eb2303036e2209f7705e7204c7ef1f624c22
Robert Francis Lynch <rofrly@utu.fi>
2018-05-30 16:47:20 +0300

    Put data in long from for time series analysis

--- lottas_3.R
+++ lottas_3.R
@@ -7,13 +7,17 @@ library(tidyr)
 p$martta<- as.numeric(p$martta)
 p$lotta<- as.numeric(p$lotta)
 p$never_married <- ifelse(is.na(p$spouse_id), 1, 0)
-# birth year must be earlier than 1920 for Katiha table and select females
+
+# deleted retarded rows
+# p <- p %>% filter (age_at_first_birth > 12 & age_at_first_birth < 51 | is.na(age_at_first_birth))
+
+
 p <- p %>% filter (sex==0)
 p$age_in_44 <- 1944-p$birthyear
 
 # filter age at first birth between 13 and 50 - i.e. get rid of rows with impossible values -deletes
 # 281 women from full data
-p <- p %>% filter (age_at_first_birth > 12 & age_at_first_birth < 51 | is.na(age_at_first_birth))
+
 p <- p %>% select ("id","lotta","age_in_44","birthyear","agriculture","education","martta","never_married")
 
 
@@ -27,14 +31,56 @@ colnames(children2)[3] <- "parentid"
 # 1) stack children so we have all ids
 children<- bind_rows(children1,children2)
 
+
 # link p to children and add column for each kids birth year
 # Year, Reproduced, Age
 
 
-data <- full_data %>% select ("per_capita_suicides","population","sanders",
-                              "clinton","Trump","Clinton")
-# tidy data - gather up the columns
-data <- gather(data, key = Candidate, value = percent,
-               sanders, clinton, Trump, Clinton)
-
 # possible DV'S kids, age at first birth and birth intervals
+#read in the data, as you normally would
+
+
+
+#make sure the individual's birth year column and death year/censored year column are numeric
+#then make a column for 'last appearance'
+# you can play around with the ages but now its just making sure they were at least 40 (and
+#had completed reproduction) when they were interviewed 
+#p<- p%>% filter (birthyear<1931)
+#p$year_age_13 <- as.numeric(p$birthyear+13)
+#p$year_age_45 <- as.numeric(p$birthyear+45)
+# drop rows with na's in the birth year column
+p<- p %>% drop_na(birthyear)
+
+p$birth_plus_13 <- p$birthyear+13
+p$lastapp <- ifelse (p$birthyear<1925, p$birthyear+45,1970)
+
+## now make cut off when you want (e.g. age 50 or ages 13-50)
+
+
+p$year <- mapply(seq, p$birth_plus_13, p$lastapp, SIMPLIFY = FALSE) 
+#Creates a 
+#sequence for each row,
+#so if birth year is 1850 and death year 1900, the cell says 1850:1900.
+#Simplify makes a matrix, but we want to keep a dataframe
+
+#unnest creates a new row for each different value within a "cell" - 
+#this is taken from the 'year' column created above
+p_long <- unnest(p, year)
+
+# Now all women are censored either at age 45 or at the year of their interview
+
+#  NEXT link their kids year of birth to their 'year' by id=parentid
+children <- children %>% select ("birthYear","parentid")
+children$id <- 1
+children<- children %>% drop_na(birthYear)
+children<- children %>% drop_na(parentid)
+twins <- p_long %>% left_join (children, by=c("id"="parentid","year"="birthYear"))
+
+# find duplicate data
+#dupes<-children[which(duplicated(children[,c('parentid','birthYear')])==T),]
+
+# make p_long_3 no duplicates for year and id
+no_twins <- twins[!duplicated(twins[,c("id","year")]),]
+
+
+# so no_twins is no multiple births in same year, and twins includes them
