a5b344986c7525931a7dbbd11c2652d02e659f03
Robert Francis Lynch <rofrly@utu.fi>
2018-05-31 14:19:12 +0300

    Add time series analysis code

--- lottas_3.R
+++ lottas_3.R
@@ -3,23 +3,30 @@ file2<- "person_data.rds"
 p <- readRDS(paste0(path, file2))
 library(dplyr)
 library(tidyr)
+library(lme4)
+library(rethinking)
 # convert booleans to numeric
 p$martta<- as.numeric(p$martta)
 p$lotta<- as.numeric(p$lotta)
 p$never_married <- ifelse(is.na(p$spouse_id), 1, 0)
-
+p$age_1945 <- 1945-p$birthyear
 # deleted retarded rows
 # p <- p %>% filter (age_at_first_birth > 12 & age_at_first_birth < 51 | is.na(age_at_first_birth))
 
 
 p <- p %>% filter (sex==0)
-p$age_in_44 <- 1944-p$birthyear
+p <- p %>% filter (age_at_first_birth > 12 & age_at_first_birth < 51 | is.na(age_at_first_birth))#80708
 
+# get all childless women in 1940 and those who never gave birth 
+#(adding kids =0 make this analysis more conservative)
+   #p <- p %>% filter (first_child_yob>1944 | ( is.na(first_child_yob) & kids==0 ))#44041
+   #p <- p %>% filter (first_child_yob>1944 )#29533
+p <- p %>% filter (first_child_yob>1944 | weddingyear>1944 )#35110
 # filter age at first birth between 13 and 50 - i.e. get rid of rows with impossible values -deletes
 # 281 women from full data
 
-p <- p %>% select ("id","lotta","age_in_44","birthyear","agriculture","education","martta","never_married")
-
+p <- p %>% select ("id","lotta","birthyear","agriculture","education","martta","never_married")
+p <- p[complete.cases(p), ]
 
 # load children data
 children <- readRDS("C:/Users/rofrly/Dropbox/Github/Lottas_2/children.rds")
@@ -30,7 +37,7 @@ colnames(children2)[3] <- "parentid"
 # put data in long form
 # 1) stack children so we have all ids
 children<- bind_rows(children1,children2)
-
+rm(children1, children2)
 
 # link p to children and add column for each kids birth year
 # Year, Reproduced, Age
@@ -45,9 +52,6 @@ children<- bind_rows(children1,children2)
 #then make a column for 'last appearance'
 # you can play around with the ages but now its just making sure they were at least 40 (and
 #had completed reproduction) when they were interviewed 
-#p<- p%>% filter (birthyear<1931)
-#p$year_age_13 <- as.numeric(p$birthyear+13)
-#p$year_age_45 <- as.numeric(p$birthyear+45)
 # drop rows with na's in the birth year column
 p<- p %>% drop_na(birthyear)
 
@@ -75,7 +79,17 @@ children$id <- 1
 children<- children %>% drop_na(birthYear)
 children<- children %>% drop_na(parentid)
 twins <- p_long %>% left_join (children, by=c("id"="parentid","year"="birthYear"))
+colnames(twins)[11] <- "reproduced"
+twins$reproduced[is.na(twins$reproduced)] <- 0
 
+twins$war_year <- ifelse(twins$year>1938 & twins$year<1946 , 1,0)
+twins$age <- twins$year-twins$birthyear
+
+
+
+# select data frame columns
+twins <- twins %>% select ("id","lotta","education","agriculture","martta","never_married","year",
+                           "reproduced","war_year","age")
 # find duplicate data
 #dupes<-children[which(duplicated(children[,c('parentid','birthYear')])==T),]
 
@@ -84,3 +98,32 @@ no_twins <- twins[!duplicated(twins[,c("id","year")]),]
 
 
 # so no_twins is no multiple births in same year, and twins includes them
+
+# run time series analysis in glmer
+model <-glmer(reproduced ~ lotta*age + education + agriculture + martta +
+                (1|year), data = no_twins, family = binomial, 
+        control = glmerControl(optimizer="nloptwrap", optCtrl=list(maxfun=100000)))
+# compare AIC scores between models droppin a variable
+m1 <- drop1(model)
+
+summary(model)
+
+# turn the formula into a map formula with glimmer
+
+data(cars)
+glimmer(reproduced ~ lotta + education + agriculture + martta + age + age*lotta +
+          (1|year) +(1|id), data = twins, family = binomial)
+
+
+model <- map2stan(alist(
+  dist ~ dnorm( mu , sigma ),
+  mu <- Intercept +
+    b_speed*speed,
+  Intercept ~ dnorm(0,10),
+  b_speed ~ dnorm(0,10),
+  sigma ~ dcauchy(0,2)
+),data=cars)
+  
+#time series example in map2stan
+intestinal_resistance ~ N( mu,sigma )
+mu= f(time,antibiotics,patient )
