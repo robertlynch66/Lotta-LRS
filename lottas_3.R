11e486cb08614829b4a2fb04b376495c19974b46
Robert Lynch <robertflynch@gmail.com>
2018-06-01 18:36:36 +0300

    Added code for some figuers graphing reproductive schedulle of lottas vs non lottas by age in 1945

--- lottas_3.R
+++ lottas_3.R
@@ -20,11 +20,11 @@ p <- p %>% filter (age_at_first_birth > 12 & age_at_first_birth < 51 | is.na(age
 #(adding kids =0 make this analysis more conservative)
    #p <- p %>% filter (first_child_yob>1944 | ( is.na(first_child_yob) & kids==0 ))#44041
    #p <- p %>% filter (first_child_yob>1944 )#29533
-p <- p %>% filter (first_child_yob>1944 | weddingyear>1944 )#35110
+p <- p %>% filter (first_child_yob>1944 )#35110
 # filter age at first birth between 13 and 50 - i.e. get rid of rows with impossible values -deletes
 # 281 women from full data
 
-p <- p %>% select ("id","lotta","birthyear","agriculture","education","martta","never_married")
+p <- p %>% select ("id","lotta","birthyear","agriculture","education","martta","never_married","age_1945")
 p <- p[complete.cases(p), ]
 
 # load children data
@@ -78,7 +78,7 @@ children$id <- 1
 children<- children %>% drop_na(birthYear)
 children<- children %>% drop_na(parentid)
 twins <- p_long %>% left_join (children, by=c("id"="parentid","year"="birthYear"))
-colnames(twins)[11] <- "reproduced"
+colnames(twins)[12] <- "reproduced"
 twins$reproduced[is.na(twins$reproduced)] <- 0
 
 twins$war_year <- ifelse(twins$year>1938 & twins$year<1946 , 1,0)
@@ -88,7 +88,7 @@ twins$age <- twins$year-twins$birthyear
 
 # select data frame columns
 twins <- twins %>% select ("id","lotta","education","agriculture","martta","never_married","year",
-                           "reproduced","war_year","age")
+                           "reproduced","war_year","age","age_1945")
 # find duplicate data
 #dupes<-children[which(duplicated(children[,c('parentid','birthYear')])==T),]
 
@@ -154,3 +154,243 @@ path<- (paste0("results/"))
 filename <- "lottas_LRS_age_and_age_sq.rds"
 
 saveRDS(model, paste0(path, filename))
+
+
+
+#### some basic graphs
+#### get the means
+#### of the reproduction across ages and age squared
+d <- no_twins %>% filter (lotta==1) %>% mean (reproduced)
+
+attach(no_twins)
+aggdata <-aggregate(no_twins, by=list(lotta,age), 
+                    FUN=mean, na.rm=TRUE)
+print(no_twins)
+detach(no_twins)
+
+y <- no_twins %>% filter (lotta==0)
+z <- no_twins %>% filter (lotta==1)
+
+m <- aggregate(reproduced ~  age, data = y, mean)
+colnames(m)[2] <- "not_lotta"
+n <- aggregate(reproduced ~  age, data = z, mean)
+colnames(n)[2] <- "lotta"
+colnames(n)[1] <- "age2"
+new <- cbind(m,n)
+#temporarily disbale scientific notation
+options("scipen"=0, "digits"=7)
+new$diff <- 10*(new$lotta- new$not_lotta)
+#new$diff <- sprintf(as.character(new$diff))
+new <- new %>% select("age","diff")
+
+ggplot(new, aes(age)) + 
+  geom_line(aes(y = lotta, colour = "lotta")) + 
+  geom_line(aes(y = not_lotta, colour = "not_lotta"))
+
+p45_to_50 <- no_twins %>% filter (year>1944 & year <1951)
+
+p <- aggregate(reproduced ~   lotta + year +age
+                 , data = p45_to_50, FUN = function(x) c(mean = mean(x), n = length(x) ) )
+
+aggregate (reproduced ~  lotta +age, data=p, FUN = function(x) c(mean = mean(x) ) )
+
+
+## try to adjust age for year
+p15 <- no_twins %>% filter (age_1945==15)
+p16 <- no_twins %>% filter (age_1945==16)
+p17 <- no_twins %>% filter (age_1945==17)
+p18 <- no_twins %>% filter (age_1945==18)
+p19 <- no_twins %>% filter (age_1945==19)
+p20 <- no_twins %>% filter (age_1945==20)
+p25 <- no_twins %>% filter (age_1945 >20 & age_1945 <25)
+p30 <- no_twins %>% filter (age_1945 >24 & age_1945 <30)
+p35 <- no_twins %>% filter (age_1945 >29 & age_1945 <35)
+p40 <- no_twins %>% filter (age_1945 >34 & age_1945 <40)
+p45 <- no_twins %>% filter (age_1945 > 39 & age_1945 <45)
+
+# get the mean reproduction for all lottas vs non lottas across years of their lives
+# for lottas who were 20,25,30,35 and 40 in 1945
+# 15 year olds
+pd <- position_dodge(0.3)
+
+library(plyr)
+
+# Run the functions length, mean, and sd on the value of "reproduced" for each group, 
+# broken down by lotta + age
+m15 <- ddply(p15, c("lotta", "age"), summarise,
+               N    = length(reproduced),
+               mean = mean(reproduced),
+               sd   = sd(reproduced),
+               se   = sd / sqrt(N)
+)
+# make lotta a factor
+m15$lotta <- as.factor(m15$lotta)
+# make a point plot with colors and 
+plot15 <- ggplot(data = m15, aes(x=age, y=mean, colour=lotta, group=lotta)) +
+  geom_point(position=pd, size=3) +
+  geom_errorbar(aes(ymin=mean-se, ymax=mean+se), colour="black", width=.1, position=pd)+
+  scale_color_manual(values=c("darkblue", "darkgreen")) + 
+  labs(x = "Age gave birth", y="mean reproduction", title="15 years old in 1945")
+plot15
+
+
+# 16
+m16 <- ddply(p16, c("lotta", "age"), summarise,
+             N    = length(reproduced),
+             mean = mean(reproduced),
+             sd   = sd(reproduced),
+             se   = sd / sqrt(N)
+)
+# make lotta a factor
+m16$lotta <- as.factor(m16$lotta)
+# make a point plot with colors and 
+plot16 <- ggplot(data = m16, aes(x=age, y=mean, colour=lotta, group=lotta)) +
+  geom_point(position=pd, size=3) +
+  geom_errorbar(aes(ymin=mean-se, ymax=mean+se), colour="black", width=.1, position=pd)+
+  scale_color_manual(values=c("darkblue", "darkgreen")) + 
+  labs(x = "Age gave birth", y="mean reproduction", title="16 years old in 1945")
+plot16
+# 17
+m17 <- ddply(p17, c("lotta", "age"), summarise,
+             N    = length(reproduced),
+             mean = mean(reproduced),
+             sd   = sd(reproduced),
+             se   = sd / sqrt(N)
+)
+# make lotta a factor
+m17$lotta <- as.factor(m17$lotta)
+# make a point plot with colors and 
+plot17 <- ggplot(data = m17, aes(x=age, y=mean, colour=lotta, group=lotta)) +
+  geom_point(position=pd, size=3) +
+  geom_errorbar(aes(ymin=mean-se, ymax=mean+se), colour="black", width=.1, position=pd)+
+  scale_color_manual(values=c("darkblue", "darkgreen")) + 
+  labs(x = "Age gave birth", y="mean reproduction", title="17 years old in 1945")
+plot17
+# 18
+m18 <- ddply(p18, c("lotta", "age"), summarise,
+             N    = length(reproduced),
+             mean = mean(reproduced),
+             sd   = sd(reproduced),
+             se   = sd / sqrt(N)
+)
+# make lotta a factor
+m18$lotta <- as.factor(m18$lotta)
+# make a point plot with colors and 
+plot18 <- ggplot(data = m18, aes(x=age, y=mean, colour=lotta, group=lotta)) +
+  geom_point(position=pd, size=3) +
+  geom_errorbar(aes(ymin=mean-se, ymax=mean+se), colour="black", width=.1, position=pd)+
+  scale_color_manual(values=c("darkblue", "darkgreen")) + 
+  labs(x = "Age gave birth", y="mean reproduction", title="18 years old in 1945")
+plot18
+# 19
+m19 <- ddply(p19, c("lotta", "age"), summarise,
+             N    = length(reproduced),
+             mean = mean(reproduced),
+             sd   = sd(reproduced),
+             se   = sd / sqrt(N)
+)
+# make lotta a factor
+m19$lotta <- as.factor(m19$lotta)
+# make a point plot with colors and 
+plot19 <- ggplot(data = m19, aes(x=age, y=mean, colour=lotta, group=lotta)) +
+  geom_point(position=pd, size=3) +
+  geom_errorbar(aes(ymin=mean-se, ymax=mean+se), colour="black", width=.1, position=pd)+
+  scale_color_manual(values=c("darkblue", "darkgreen")) + 
+  labs(x = "Age gave birth", y="mean reproduction", title="19 years old in 1945")
+plot19
+# 20 year olds first
+m20 <- ddply(p20, c("lotta", "age"), summarise,
+             N    = length(reproduced),
+             mean = mean(reproduced),
+             sd   = sd(reproduced),
+             se   = sd / sqrt(N)
+)
+# make lotta a factor
+m20$lotta <- as.factor(m20$lotta)
+# make a point plot with colors and 
+plot20 <- ggplot(data = m19, aes(x=age, y=mean, colour=lotta, group=lotta)) +
+  geom_point(position=pd, size=3) +
+  geom_errorbar(aes(ymin=mean-se, ymax=mean+se), colour="black", width=.1, position=pd)+
+  scale_color_manual(values=c("darkblue", "darkgreen")) + 
+  labs(x = "Age gave birth", y="mean reproduction", title="20 years old in 1945")
+plot20
+# 25 year olds
+m25 <- ddply(p25, c("lotta", "age"), summarise,
+             N    = length(reproduced),
+             mean = mean(reproduced),
+             sd   = sd(reproduced),
+             se   = sd / sqrt(N)
+)
+# make lotta a factor
+m25$lotta <- as.factor(m25$lotta)
+# make a point plot with colors and 
+plot25 <- ggplot(data = m25, aes(x=age, y=mean, colour=lotta, group=lotta)) +
+  geom_point(position=pd, size=3) +
+  geom_errorbar(aes(ymin=mean-se, ymax=mean+se), colour="black", width=.1, position=pd)+
+  scale_color_manual(values=c("darkblue", "darkgreen")) + 
+  labs(x = "Age gave birth", y="mean reproduction", title="21-25 years old in 1945")
+plot25
+# 30 year olds
+m30 <- ddply(p30, c("lotta", "age"), summarise,
+             N    = length(reproduced),
+             mean = mean(reproduced),
+             sd   = sd(reproduced),
+             se   = sd / sqrt(N)
+)
+# make lotta a factor
+m30$lotta <- as.factor(m30$lotta)
+# make a point plot with colors and 
+plot30 <- ggplot(data = m30, aes(x=age, y=mean, colour=lotta, group=lotta)) +
+  geom_point(position=pd, size=3) +
+  geom_errorbar(aes(ymin=mean-se, ymax=mean+se), colour="black", width=.1, position=pd)+
+  scale_color_manual(values=c("darkblue", "darkgreen")) + 
+  labs(x = "Age gave birth", y="mean reproduction", title="26-30 years old in 1945")
+plot30
+# 35 year olds
+m35 <- ddply(p35, c("lotta", "age"), summarise,
+             N    = length(reproduced),
+             mean = mean(reproduced),
+             sd   = sd(reproduced),
+             se   = sd / sqrt(N)
+)
+# make lotta a factor
+m35$lotta <- as.factor(m35$lotta)
+# make a point plot with colors and 
+plot35 <- ggplot(data = m35, aes(x=age, y=mean, colour=lotta, group=lotta)) +
+  geom_point(position=pd, size=3) +
+  geom_errorbar(aes(ymin=mean-se, ymax=mean+se), colour="black", width=.1, position=pd)+
+  scale_color_manual(values=c("darkblue", "darkgreen")) + 
+  labs(x = "Age gave birth", y="mean reproduction", title="31-35 years old in 1945")
+plot35
+# 40 year olds
+m40 <- ddply(p40, c("lotta", "age"), summarise,
+             N    = length(reproduced),
+             mean = mean(reproduced),
+             sd   = sd(reproduced),
+             se   = sd / sqrt(N)
+)
+# make lotta a factor
+m40$lotta <- as.factor(m40$lotta)
+# make a point plot with colors and 
+plot40 <- ggplot(data = m40, aes(x=age, y=mean, colour=lotta, group=lotta)) +
+  geom_point(position=pd, size=3) +
+  geom_errorbar(aes(ymin=mean-se, ymax=mean+se), colour="black", width=.1, position=pd)+
+  scale_color_manual(values=c("darkblue", "darkgreen")) + 
+  labs(x = "Age gave birth", y="mean reproduction", title="36-40 years old in 1945")
+plot40
+# 45 year olds
+m45 <- ddply(p45, c("lotta", "age"), summarise,
+             N    = length(reproduced),
+             mean = mean(reproduced),
+             sd   = sd(reproduced),
+             se   = sd / sqrt(N)
+)
+# make lotta a factor
+m45$lotta <- as.factor(m45$lotta)
+# make a point plot with colors and 
+plot45 <- ggplot(data = m45, aes(x=age, y=mean, colour=lotta, group=lotta)) +
+  geom_point(position=pd, size=3) +
+  geom_errorbar(aes(ymin=mean-se, ymax=mean+se), colour="black", width=.1, position=pd)+
+  scale_color_manual(values=c("darkblue", "darkgreen")) + 
+  labs(x = "Age gave birth", y="mean reproduction", title="Over 40 years old in 1945")
+plot45
